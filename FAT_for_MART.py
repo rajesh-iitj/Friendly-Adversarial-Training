da2e75a79c954b4aad13967c657953e005710cc9
Rajesh Mohan <m22aie221@iitj.ac.in>
2023-09-13 11:59:29 +0530

    caltech 101 train test split

--- FAT_for_MART.py
+++ FAT_for_MART.py
@@ -145,9 +145,9 @@ if args.dataset == "svhn":
     test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 
 if args.dataset == "caltech101":
-    trainset = torchvision.datasets.Caltech101(root='./data', split='train', download=True, transform=transform_train)
+    trainset = torchvision.datasets.Caltech101(root='./data', download=True, transform=transform_train)
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-    testset = torchvision.datasets.Caltech101(root='./data', split='test', download=True, transform=transform_test) 
+    testset = torchvision.datasets.Caltech101(root='./data', download=True, transform=transform_test) 
     test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 
 print('==> Load Model')
