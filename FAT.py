801928a9c4284566f7a1246eea8d9fc9c5fd8104
Rajesh Mohan <m22aie221@iitj.ac.in>
2023-09-13 19:04:16 +0530

    STL10 correcton in data loading

--- FAT.py
+++ FAT.py
@@ -147,10 +147,6 @@ if args.dataset == "caltech101":
     test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
 
 if args.dataset == "STL10":
-    trainset = torchvision.datasets.Caltech101(root='./data', download=True, transform=transform_train)
-    train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-    testset = torchvision.datasets.Caltech101(root='./data', download=True, transform=transform_test) 
-    test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=False, num_workers=2)
     # Load the STL-10 dataset (specify root directory where data is located)
     train_dataset = STL10(root='stl10_data', split='train', transform=transform_train, download=False)
     test_dataset = STL10(root='stl10_data', split='test', transform=transform_test, download=False)
